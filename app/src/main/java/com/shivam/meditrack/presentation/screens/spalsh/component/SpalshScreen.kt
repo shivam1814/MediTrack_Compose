15dc6a843988c559725fc327a375fd5c06dbe8ae
Shivam Mandalia <mandaliashivam72@gmail.com>
2026-05-05 22:45:58 +0530

    continue building splash screen commit by shivam

--- app/src/main/java/com/shivam/meditrack/presentation/screens/spalsh/component/SpalshScreen.kt
+++ app/src/main/java/com/shivam/meditrack/presentation/screens/spalsh/component/SpalshScreen.kt
@@ -1,14 +1,24 @@
 package com.shivam.meditrack.presentation.screens.spalsh.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.shivam.meditrack.R
+import com.shivam.meditrack.ui.theme.Spacing
 import com.shivam.meditrack.ui.theme.primary
 import com.shivam.meditrack.ui.theme.white
 
@@ -23,8 +33,45 @@ fun SplashScreen() {
                 brush = Brush.verticalGradient(
                     colors = listOf(primary, white)
                 )
-            ).systemBarsPadding(),
+            )
+            .systemBarsPadding(),
     ) {
+
+        Box(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+
+            Image(
+                contentDescription = null,
+                painter = painterResource(R.drawable.ic_capsule_light_back),
+                modifier = Modifier
+                    .size(Spacing.s250)
+                    .align(Alignment.TopEnd)
+                    .offset(x = Spacing.s75)
+            )
+
+            Image(
+                contentDescription = null,
+                painter = painterResource(R.drawable.ic_plus_light_back),
+                modifier = Modifier
+                    .padding(top = Spacing.s210)
+                    .size(Spacing.s280)
+                    .offset(x = -Spacing.s75)
+            )
+
+
+            Image(
+                contentDescription = null,
+                painter = painterResource(R.drawable.ic_capsule_logo),
+                modifier = Modifier
+                    .padding(top = Spacing.s80)
+                    .size(Spacing.s210)
+                    .align(Alignment.TopCenter)
+            )
+
+
+        }
+
 
     }
 }
--- app/src/main/java/com/shivam/meditrack/ui/theme/Spacing.kt
+++ app/src/main/java/com/shivam/meditrack/ui/theme/Spacing.kt
@@ -12,4 +12,24 @@ object Spacing {
     val s16: Dp = 16.dp
     val s24: Dp = 24.dp
     val s32: Dp = 32.dp
+    val s36: Dp = 36.dp
+    val s40: Dp = 40.dp
+    val s44: Dp = 44.dp
+    val s48: Dp = 48.dp
+    val s52: Dp = 52.dp
+    val s54: Dp = 54.dp
+    val s60: Dp = 60.dp
+    val s75:Dp = 75.dp
+    val s80:Dp = 80.dp
+    val s150:Dp = 150.dp
+    val s100:Dp = 100.dp
+    val s160:Dp = 160.dp
+    val s180: Dp = 180.dp
+    val s200: Dp = 200.dp
+    val s204: Dp = 204.dp
+    val s210:Dp = 210.dp
+    val s250: Dp = 250.dp
+    val s260: Dp = 260.dp
+    val s270: Dp = 270.dp
+    val s280: Dp = 280.dp
 }
